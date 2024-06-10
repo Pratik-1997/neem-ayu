39530a22d8b2d9e1b43c15ffda3ba6eb78fdbcbc
Pratik-1997 <pratikkeluskar1997@gmail.com>
2024-06-10 23:18:43 +0530

    tes

--- main.js
+++ main.js
@@ -9,3 +9,55 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+      if ($(window).width() < 480) {
+        new Swiper(".testimonial-swiper", {
+          slidesPerView: 1,
+          pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+
+        new Swiper(".feature-prod-swiper", {
+          slidesPerView: 1,
+          pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+      } else {
+        new Swiper(".testimonial-swiper", {
+          slidesPerView: 3,
+          pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+
+        new Swiper(".feature-prod-swiper", {
+          slidesPerView: 4,
+          pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+      }
+
